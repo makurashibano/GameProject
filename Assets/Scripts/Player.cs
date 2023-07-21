e808b2064f94c7b4b3067805b9f3d7e4b3b9e077
sPtn <32430755+deelsobe@users.noreply.github.com>
2023-07-21 14:04:04 +0900

    プレイヤーの移動を付けました
    まだ仮です。
    Velocityで動いています。

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0;
+
+
+    // Rigidbodyコンポーネントを入れる変数"rb"を宣言する。 
+	public Rigidbody rb; 
+	void Start() { 
+		// Rigidbodyコンポーネントを取得する 
+		rb = GetComponent<Rigidbody>(); 
+	}
+	void FixedUpdate() { 
+		//  Upキーで上昇 
+		if (Input.GetKey("up")) { 
+			rb.velocity = new Vector3(0, 0, speed); 
+		} 
+		// Downキーで下降 
+		if (Input.GetKey("down")) { 
+			rb.velocity = new Vector3(0, 0, -speed); 
+		} 
+		// right キーで右に進む 
+		if (Input.GetKey("right")) { 
+			rb.velocity = new Vector3(speed, 0, 0); 
+		} 
+		//left キーで左に進む 
+		if (Input.GetKey("left")) { 
+			rb.velocity = new Vector3(-speed, 0, 0); 
+		} 
+	}
+}
+
