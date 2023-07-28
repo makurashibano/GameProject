05f4338860b3880611bf33414adb56e57da13158
makurashibano <91774182+makurashibano@users.noreply.github.com>
2023-07-28 10:01:58 +0900

    近いキャラクターを追いかける

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,18 +6,37 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
 	//playerの数を入れる配列
-	public GameObject[] gm;
+	GameObject[] targets;
+    GameObject target;
 	NavMeshAgent navmesh; 
     // Start is called before the first frame update
     void Start()
     {
         navmesh = GetComponent<NavMeshAgent>();
+        targets = GameObject.FindGameObjectsWithTag("Player");
     }
 
+    private void  FetchNearObjectWithTag(string tagName)
+    {
+        // 該当タグが1つしか無い場合はそれを返す
+        targets = GameObject.FindGameObjectsWithTag(tagName);
+        GameObject result = null;
+        var minTargetDistance = float.MaxValue;
+        for(int i = 0; i<targets.Length;i++)
+        {
+            if (this.gameObject == targets[i]) continue;
+            // 前回計測したオブジェクトよりも近くにあれば記録
+            var targetDistance = Vector3.Distance(transform.position, targets[i].transform.position);
+            if (!(targetDistance < minTargetDistance)) continue;
+            minTargetDistance = targetDistance;
+            target = targets[i];
+        }
+    }
     // Update is called once per frame
     void Update()
     {
+        FetchNearObjectWithTag("Player");
 		//playerをNavmeshを使って追いかける
-        navmesh.destination = gm[0].transform.position;
+        navmesh.destination = target.transform.position;
     }
 }
