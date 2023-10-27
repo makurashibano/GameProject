fe5e914f548e76d3019a0e941d5288dd06d89f35
potato893 <ooa226di0001@edu.oas.ac.jp>
2023-10-27 16:11:05 +0900

    床上下(仮)

--- Assets/Scripts/TransformPlane.cs
+++ Assets/Scripts/TransformPlane.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class TransformPlane : MonoBehaviour
 {
+    float y;
+    Vector3 pos;
     // Start is called before the first frame update
     void Start()
     {
         //ステージのPlaneオブジェクト上下
+        pos = transform.position;
+        y = 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+        if (y >= 2 || y<0)
+        {
+            y += -1f * Time.deltaTime;
+        }
+        else if(y <= -2 || y > 0)
+        {
+            y += 1f * Time.deltaTime;
+        }
         
+
+        transform.position = new Vector3(0, y+1.5f, 0);
     }
 }
