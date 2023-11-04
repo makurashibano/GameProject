3375e51ca7953ad06bd860c3ceb80697d8736147
S <91774172+shirahamaw-19450401@users.noreply.github.com>
2023-11-04 15:16:44 +0900

    リザルトで順位順に並ぶようになるはずです

--- Assets/Scripts/Resultrank.cs
+++ Assets/Scripts/Resultrank.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Resultrank : MonoBehaviour
 {
+    [SerializeField]
+    List<GameObject> playerprefab;
+    [SerializeField]
+    List<Transform> playerrank;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < PlayerKillZone.rank.Count; i++)
+        {
+            int pl = PlayerKillZone.rank[i];
+            Instantiate(playerprefab[pl], playerrank[i]);
+        }
     }
 
     // Update is called once per frame
