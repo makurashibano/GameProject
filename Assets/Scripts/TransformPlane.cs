79695e493b6b1cc72832c5c99c8e7b3eb871b3a8
potato893 <ooa226di0001@edu.oas.ac.jp>
2023-11-04 14:27:17 +0900

    ステージ上下

--- Assets/Scripts/TransformPlane.cs
+++ Assets/Scripts/TransformPlane.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class TransformPlane : MonoBehaviour
 {
-    float y;
-    Vector3 pos;
+    float yOrigin;
+    [SerializeField]
+    float amplitude=1.5f;
+    [SerializeField]
+    float moveSpeed = 0.3f;
     // Start is called before the first frame update
     void Start()
     {
         //ステージのPlaneオブジェクト上下
-        pos = transform.position;
-        y = 0.1f;
+        yOrigin = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        if (y >= 2 || y<0)
+        /*
+        if (y >= 2 || y < 0)
         {
             y += -1f * Time.deltaTime;
         }
@@ -29,5 +31,8 @@ public class TransformPlane : MonoBehaviour
         
 
         transform.position = new Vector3(0, y+1.5f, 0);
+        */
+        float y=Mathf.Sin(Time.time*moveSpeed)*amplitude;
+        transform.position = new Vector3(transform.position.x, y+yOrigin,transform.position.z);
     }
 }
