818ec808ab230e37056a0edfbc8d1b1787d9a8c4
sPtn <32430755+deelsobe@users.noreply.github.com>
2023-12-09 16:34:02 +0900

    プッシュするためにプッシュする

--- Assets/Scripts/TimeManagement.cs
+++ Assets/Scripts/TimeManagement.cs
@@ -9,11 +9,21 @@ public class TimeManagement : MonoBehaviour
     [SerializeField]
     float countdownTime = 0f;
     float CountDown = 0f;
+    float StartCount = 5f;
     public TextMeshProUGUI TimerText;
     public GameObject TimeUpPanel;
-    bool sceneActive = true;
+
+// <<<<<<< HEAD
+
+    bool sceneActive = false;
+    bool GameStart = true;
+
+// =======
+    
     public bool isdrawStopTime = false;
         
+//  >>>>>>> main
+
     void ReturnToTitle()
     {
         SceneManager.LoadScene("title");
@@ -26,10 +36,52 @@ public class TimeManagement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (GameStart == true)
+        {
+            
+
+            StartCount -= Time.deltaTime;
+            // 試合時間のやつもカウントダウンだから、5秒間だけ数を数えて下のコードに渡すみたいな処理を作りたい
+            // たぶんそうすればこっちの処理を先にするから、結果的に誰も動かない時間になるんじゃないのか？
+            // プレイヤーキャラが生成されるのはどのタイミング？
+            
+            int second = (int)StartCount;
+
+
+            // あとはここにカウントダウンを５秒から始めるやつがあれば完璧
+
+
+            TimerText.text = second.ToString();
+
+            if(StartCount <= 0)
+            {
+                TimerText.text = "start!";
+                // こうやったら３秒だけ表示して開始するんじゃね？
+
+                if (StartCount <= -3)
+                {
+                    sceneActive = true;
+                    GameStart = false;
+                    // -3秒になったときに１エリア下のコードに飛べるならなんでもいい。
+                    // breakみたいな機能を持ってるやつない？
+                } 
+            }
+        }
+        
+
+
+
+        // CountDownが1以下になるとreturnってどこに行くんだ？
         if (CountDown <= 1f)
         {
             return;
         }
+
+
+        // sceneActiveってなんだろう、これfalseにしとけばタイマー止まるか？
+        // StopTimerでfalseになってるし多分、なる
+        // Playerたちが操作できないようにするのは後で考える？
+
         if (sceneActive==true)
         {
             CountDown -= Time.deltaTime;
