1e948626fc293f900f650a1651f097e644f09a42
makurashibano <91774182+makurashibano@users.noreply.github.com>
2024-01-12 12:20:07 +0900

    いったん保存

--- Assets/Scripts/PlayerKillZone.cs
+++ Assets/Scripts/PlayerKillZone.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class PlayerKillZone : MonoBehaviour
 {
+    public int particlenumber = 0;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             //プレイヤーが落ちた場所にパーティクルを出現
-            GameObject particle = Instantiate(other.gameObject.GetComponent<Player>().playerParticles[0], other.gameObject.transform.position, Quaternion.Euler(90, 0, 0));
+            GameObject particle = Instantiate(other.gameObject.GetComponent<Player>().playerParticles[particlenumber], other.gameObject.transform.position, Quaternion.Euler(90, 0, 0));
             Destroy(particle, 2f);
         }
     }
